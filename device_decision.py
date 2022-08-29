527437529c33f9da65c141a959fe5f4b635396e8
Peter Belcak <belcak@ethz.ch>
2022-08-29 14:30:16 +0200

    cuda fix

--- device_decision.py
+++ device_decision.py
@@ -2,5 +2,5 @@ import torch
 torch.set_default_dtype(torch.float64)
 
 # Get cpu or gpu device for training.
-device = "cude" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Device used: {device}")
